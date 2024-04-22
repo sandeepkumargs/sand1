3bae114c06629dd3a8acbf277e610397363d259d
sandeepkumargoi <sandeep.kumar@thegoacademy.com>
2024-04-22 12:04:26 +0530

    sandeep

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
+// You installed the `express` library earlier. For more information, see "[JavaScript example: Install dependencies](#javascript-example-install-dependencies)."
 const express = require('express');
+
+// This initializes a new Express application.
 const app = express();
-app.use(express.json());
- 
 
-app.get('/',
-(req, res) => {
-    res.send('Server running on 127.0.0.1 on port 3000');
+// This defines a POST route at the `/webhook` path. This path matches the path that you specified for the smee.io forwarding. For more information, see "[Forward webhooks](#forward-webhooks)."
+//
+// Once you deploy your code to a server and update your webhook URL, you should change this to match the path portion of the URL for your webhook.
+app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+
+  // Respond to indicate that the delivery was successfully received.
+  // Your server should respond with a 2XX response within 10 seconds of receiving a webhook delivery. If your server takes longer than that to respond, then GitHub terminates the connection and considers the delivery a failure.
+  response.status(202).send('Accepted');
+
+  // Check the `x-github-event` header to learn what event type was sent.
+  const githubEvent = request.headers['x-github-event'];
+
+  // You should add logic to handle each event type that your webhook is subscribed to.
+  // For example, this code handles the `issues` and `ping` events.
+  //
+  // If any events have an `action` field, you should also add logic to handle each action that you are interested in.
+  // For example, this code handles the `opened` and `closed` actions for the `issue` event.
+  //
+  // For more information about the data that you can expect for each event type, see "[AUTOTITLE](/webhooks/webhook-events-and-payloads)."
+  if (githubEvent === 'issues') {
+    const data = request.body;
+    const action = data.action;
+    if (action === 'opened') {
+      console.log(`An issue was opened with this title: ${data.issue.title}`);
+    } else if (action === 'closed') {
+      console.log(`An issue was closed by ${data.issue.user.login}`);
+    } else {
+      console.log(`Unhandled action for the issue event: ${action}`);
+    }
+  } else if (githubEvent === 'ping') {
+    console.log('GitHub sent the ping event');
+  } else {
+    console.log(`Unhandled event: ${githubEvent}`);
+  }
 });
 
+// This defines the port where your server should listen.
+// 3000 matches the port that you specified for webhook forwarding. For more information, see "[Forward webhooks](#forward-webhooks)."
+//
+// Once you deploy your code to a server, you should change this to match the port where your server is listening.
+const port = 3000;
 
-app.get('/getexample',
-(req, res) => {
-
-    // Access the provided 'page' and 'limt' query parameters
-    let studentName = req.query.name;
-    
-    console.log("param is ",studentName);
-
-    const students = [
-        { name: 'Hari', grade: 96 },
-        { name: 'Bob', grade: 84 },
-        { name: 'Alex', grade: 100 },
-        { name: 'Sam', grade: 65 },
-        { name: 'Bobo', grade: 90 }
-    ];
-
-    let found = null;
-    students.forEach(element => {
-        if (element.name==studentName)
-        {
-            found = element;
-            console.log("Found param",studentName);
-        }
-   });
-
-    res.send(found);
+// This starts the server and tells it to listen at the specified port.
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-let rank = 1;
-
-app.post('/postexample',
-(req, res) => {
-    const { username, password } = req.body;
-    console.log("posted params", username,password);
-    const { authorization } = req.headers;
-    const dateTimeObject = new Date();
-    rank++;
-    res.send(
-        {
-            "username":"Hi There" + username,
-            "rank" : rank,
-        });
-});
- 
-app.listen(3000,
-    () => {
-        console.log(
-            'Our express server is up on port 3000'
-        );
-    });
