1ba1d0b881c62eeffe372da9e513ffa19018abc3
sandeepkumargoi <sandeep.kumar@thegoacademy.com>
2024-04-23 12:15:03 +0530

    example

---
+++ routes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.send("Welcome to the Webhooks API");
+});
+
+router.post("/log-github-webhook", async function(req, res) {
+  //console.log(req.body);
+
+  const payload = req.body;
+
+  let webhook_info = {
+    repo : payload.repository.name,
+    author : payload.sender.login,
+    time : payload.head_commit.timestamp
+  }
+  
+  const save_webhook = await req.db
+  .collection("webhooks")
+  .insertOne(webhook_info);
+
+  res.status(201).send({
+    message: "Webhook Event successfully logged"
+  });
+});
+
+router.get("/fetch-webhooks-logs", async function(req, res) {
+  console.log(req.body);
+  
+  const webhooks = await req.db
+  .collection("webhooks")
+  .find()
+  .toArray();
+
+  res.status(200).send(webhooks);
+});
+
+module.exports = router;
